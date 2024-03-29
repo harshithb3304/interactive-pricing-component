cad7d3e3fdd2ac91c899981179395af5cf2c4d16
harshithb3304 <harsh03032004@gmail.com>
2024-03-29 22:01:55 +0530

    tick mark

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -32,7 +32,7 @@ const Slider: React.FC<SliderProps> = ({ value, onChange }) => {
           border-radius: 50%;
           cursor: pointer;
           box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
-          background: url('/src/assets/icon-slider.svg') no-repeat; 
+          background: url("data:image/svg+xml;charset=utf8,%3Csvg xmlns='http://www.w3.org/2000/svg' width='22' height='13'%3E%3Cg fill='%2380FFF3' fill-rule='evenodd'%3E%3Cpath d='M16 2.558v7.884a1 1 0 001.735.679l3.639-3.943a1 1 0 000-1.356l-3.64-3.943A1 1 0 0016 2.558zM6 2.558v7.884a1 1 0 01-1.735.679L.626 7.178a1 1 0 010-1.356l3.64-3.943A1 1 0 016 2.558z'/%3E%3C/g%3E%3C/svg%3E") no-repeat; 
           background-position: center;
           background-color: hsl(174, 86%, 45%); 
         }
