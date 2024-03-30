d9c5e62465b5c4a313b81e9cae3ee9945f7804a4
harshithb3304 <harsh03032004@gmail.com>
2024-03-30 14:40:56 +0530

    dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -51,7 +51,7 @@ function App() {
         ) : (
           <>
             <svg
-              class="w-6 h-6 text-gray-800 dark:text-white"
+              className="w-6 h-6 text-gray-800 dark:text-white"
               aria-hidden="true"
               xmlns="http://www.w3.org/2000/svg"
               width="24"
